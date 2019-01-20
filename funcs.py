44050716fd50aed3757c0a5e1794e6f6d043499a
Dan Hewer <dan@haphazardhacking.co.uk>
2019-01-20 23:40:12 +0000

    added funcs file for definitions

---
+++ funcs.py
@@ -0,0 +1,26 @@
+﻿def between(value, a, b):
+    pos_a = value.find(a)
+    if pos_a == -1: 
+        return ""
+    pos_b = value.rfind(b)
+    if pos_b == -1: 
+        return ""
+    adjusted_pos_a = pos_a + len(a)
+    if adjusted_pos_a >= pos_b: 
+        return ""
+    return value[adjusted_pos_a:pos_b]
+
+def before(value, a):
+    pos_a = value.find(a)
+    if pos_a == -1: 
+        return ""
+    return value[0:pos_a]
+
+def after(value, a):
+    pos_a = value.rfind(a)
+    if pos_a == -1: 
+        return ""
+    adjusted_pos_a = pos_a + len(a)
+    if adjusted_pos_a >= len(value): 
+        return ""
+    return value[adjusted_pos_a:]
--- organise.py
+++ organise.py
@@ -1,29 +1,32 @@
-import os
+﻿import os
 import sys
+import funcs
+import gzip
 
 if len(sys.argv) < 2:
-    print("Requires at least one argument to be specified. e.g. domaindumpcheck <domainlist.csv> <start_directory>(optional)")
+    print("Please secify the root directory containing the files you wish to organise")
 else:
-    domain_list = sys.argv[1]
-    if len(sys.argv) > 2:
-        root_dir = sys.argv[2]
-    else:
-        root_dir = "/"
-    with open(domain_list,r) as domain:
-        for line in domains:
-            keyword = line
-            for root, dirs, files in os.walk(root_dir, onerror=None):  
-                for filename in files:  
-                    file_path = os.path.join(root, filename)  
-                    try:
-                        with open(file_path, "rb") as f:  
-                            for line in f:  
-                                try:
-                                    line = line.decode("utf-8")  
-                                except ValueError:  
-                                    continue
-                                if keyword in line:
-                                    print(file_path)  
-                                    break  
-                    except (IOError, OSError):
-                        pass
+    #step 1 - organise infile files based on domain name. max file size - 4GB
+    #step 2 - compress all original files with gzip
+    # with zip.open as newfile
+    # size_buffer = 0
+    #for each unsorted file
+    # file_size = os.path.getsize(file)
+    # if (size_buffer + file_size) > 1073741824:
+    #   break, 
+    #else
+    root_dir = sys.argv[1]
+    for root, dirs, files in os.walk(root_dir, onerror=None):  
+        for filename in files:  
+            unsorted_file = os.path.join(root, filename)  
+            try:
+                with open(unsorted_file, "rb") as in_file:
+                    count = 0
+                    for line in in_file:  
+                        try:
+                            line = line.decode("utf-8")  
+                        except ValueError:  
+                            continue
+                        domain = funcs.between(line,"@",":")
+            except (IOError, OSError):
+                pass
