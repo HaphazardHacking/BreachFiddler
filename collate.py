6e1cdeece3e1c8910b0399650ac9c49d9c95badb
Dan Hewer <dan@haphazardhacking.co.uk>
2019-01-21 20:10:42 +0000

    decomp = working!

---
+++ collate.py
@@ -0,0 +1,32 @@
+﻿import os
+import sys
+import funcs
+
+if len(sys.argv) < 2:
+    print("Please specify the root directory containing the files you wish to collate")
+else:
+    size_buffer = 0
+    file_num_count = 0
+    root_dir = sys.argv[1]
+    for root, dirs, files in os.walk(root_dir, onerror=None):  
+        for filename in files:  
+            unsorted_file = os.path.join(root, filename)
+            file_size = os.path.getsize(unsorted_file)
+            size_buffer = size_buffer + file_size
+            file_num_count_str = str(file_num_count)
+            out = os.path.join(root,file_num_count_str)
+            with open(out, "a") as out_file: 
+                if size_buffer < 1073741824:
+                    #do stuff and write into file 
+                else:
+                    size_buffer = file_size
+                try:
+                    with open(unsorted_file, "rb") as in_file:
+                        count = 0
+                        for line in in_file:  
+                            try:
+                                line = line.decode("utf-8")  
+                            except ValueError:  
+                                continue
+                except(IOError, OSError):
+                    pass
---
+++ decomp.py
@@ -0,0 +1,23 @@
+﻿import os
+import sys
+import tarfile
+
+if len(sys.argv) < 2:
+    print("Please specify the root directory containing the files you wish to organise")
+else:
+    root_dir = sys.argv[1]
+    os.chdir(root_dir)
+    for root, dirs, files in os.walk(root_dir, onerror=None):  
+        for filename in files: 
+            unsorted_file = os.path.join(root, filename)  
+            if unsorted_file.endswith(".tar.gz"):
+                try:
+                    with tarfile.open(unsorted_file) as in_tar_file:
+                        print("Uncompressing: " + unsorted_file)
+                        in_tar_file.extractall()
+                        print("Successfully Extracted: " + unsorted_file)
+                    os.remove(unsorted_file)
+                    print("Cleaned up: " + unsorted_file)
+                except (IOError, OSError):
+                    print("Unable to read: " + unsorted_file)
+                    pass
